af85d40e0ce6d693bcc30afece9ba3e9df3cdf88
ParkAyun <iamparkayun@gmail.com>
2014-05-19 14:13:44 +0900

    Add db connection in app init

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,20 @@
 # -*- coding:utf-8 -*-
 from flask import Flask
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
 
 app = Flask(__name__)
 
+engine = create_engine('db_path', convert_unicode=True)
+db_session = scoped_session(sessionmaker(autocommit=True,
+                                         autoflush=False,
+                                         bind=engine))
+
+Base = declarative_base()
+Base.query = db_session.query_property()
 
 def create_app():
 
