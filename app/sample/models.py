eb0eb814aa4f6eec182f352c4ddb441df356474c
ParkAyun <iamparkayun@gmail.com>
2014-05-19 14:17:14 +0900

    rename sample table name

--- app/sample/models.py
+++ app/sample/models.py
@@ -7,7 +7,7 @@ from .. import Base
 
 
 class SampleModel(Base):
-    __tablename__ = 'hearts'
+    __tablename__ = 'sample'
     id = Column(Integer, primary_key=True)
     created_at = Column(DateTime, default=datetime.datetime.now())
     updated_at = Column(DateTime, default=datetime.datetime.now(), onupdate=datetime.datetime.now())
